03611590b155ab08c24dd131617c5bd702289804
adrian-albanes <fantabulosoadriangmail.com>
2022-09-17 19:08:51 -0300

    version1.1

---
+++ app/src/main/java/com/programacionandroid/myapplication/principal.java
@@ -0,0 +1,15 @@
+package com.programacionandroid.myapplication;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class principal extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+    }
+}
